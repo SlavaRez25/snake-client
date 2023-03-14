08197804b7c17f71e79bb49f6704841e2ecaad26
Slava <reznik.slava@gmail.com>
2023-03-14 03:58:19 +0000

    Introduced stdin func

--- play.js
+++ play.js
@@ -3,7 +3,26 @@
 const connect = require('./client')
 
 
+// setup interface to handle user input from stdin
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (keyPress) {
+  console.log('User pressed key.', keyPress);
+  if (keyPress === '\u0003') {
+    process.exit();
+  }
+};
+
+const stdin = setupInput();
+
+stdin.on("data", handleUserInput);
 
 
 
